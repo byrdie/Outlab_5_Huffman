b6f2e3d7d6f1ab536f62dc5cea1efc50e5178bb0
byrdie <byrdie@BATTLESTATION>
2014-04-06 22:36:37 -0600

    first commit!

---
+++ CSCI232_Outlab_5_Huffman/src/csci232_outlab_5_huffman/Driver.java
@@ -0,0 +1,76 @@
+
+package csci232_outlab_5_huffman;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Roy Smart
+ * CSCI 232 Data Structures & Algorithms
+ * Reads file, compresses it, and then decompresses
+ */
+public class Driver {
+    static String inputPath = "input.txt";
+    static int startChar = (int)' ';
+    static int endChar = (int)'~';
+    static int treeSize =  endChar - startChar;
+    
+    public static void main(String[] args) {
+       char[] uncrypt = readFile(inputPath);
+       /*print file*/
+       for(int i = 0; i < uncrypt.length; i++){
+           if(i % 80 == 0) System.out.println();
+           System.out.print(uncrypt[i]);
+       }
+       System.out.println();
+       
+       int[] charFreq = calculateFrequency(uncrypt);
+       printHistogram(charFreq, uncrypt.length);
+    }
+    
+    public static char[] readFile(String path){
+        String sequence = "";
+        try{
+            Scanner scanner = new Scanner(new File(path));
+            
+            while(scanner.hasNext()){
+                sequence = sequence + scanner.nextLine();
+            }                        
+        }
+        catch(FileNotFoundException e){
+            System.out.print("Input file not found...");
+        }
+        return sequence.toCharArray();
+    }
+    
+    public static int[] calculateFrequency(char[] string){
+        int[] frequency = new int[treeSize];
+        for(int i = 0; i < string.length; i++){
+            int charIndex = string[i] - startChar;
+            frequency[charIndex]++; 
+        }
+        return frequency;
+    }
+    
+    public static void printHistogram(int[] frequency, int size){
+        for(int i = startChar; i <= endChar; i++){
+            System.out.print((char)i + " ");
+        }
+        System.out.println();
+        int increment = size / 100;
+        for(int i = 1; i < 11; i++){
+            int level = increment * i;
+            for(int j = 0; j < treeSize; j++){
+                if(frequency[j] > level){
+                    System.out.print("# ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
